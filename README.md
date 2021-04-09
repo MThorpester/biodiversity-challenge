cf0520cb0ce84af836ed2499cabb7b574bc661aa
Margaret Thorpe <mthorpester@gmail.com>
2021-04-08 17:13:22 -0700

    Cleaned up code and updated the readme file.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # biodiversity-challenge
+This project uses Plotly, Javascript, D3.js. and HTML to builds an interactive dashboard for exploring the Belly Button Biodiversity dataset.
+The dashboard can be accessed [here](http://robdunnlab.com/projects/belly-button-biodiversity/ "The Public Science Lab").
+## Dataset
+This dataset catalogs the microbes that colonize human navels and is fully described [here](http://robdunnlab.com/projects/belly-button-biodiversity/ "The Public Science Lab"). The Belly Button Biodiversity study detected over 2300 species in the navels of 60 test subjects. Eight of those species were frequent and abundant across the individuals sampled (they were present in more than 70% of people); the remaining species were quite rare, often appearing in just a single navel.
+## Dashboard
+The dashboard enables users to explore the 60 samples taken from test subjects during the study. It provides information about the test subjects, as well as a detailed breakdown of the microbes present in each sample. The dashboard is made up of:
+- a picker for selecting a specific sample to explore,
+- a card displaying information about the test subject,
+- an interactive Bar chart that displays the top 10 microbes present in the selected sample,
+- a Gauge chart that shows how frequently the test subject washes their belly button, and
+- an interactive Bubble chart that shows the full microbial composition of the sample.  
+## Project Artifacts
+The project consists of:
+- a Javascript file (app.js)
+- an HTML file (index.html)
+- a file of test samples (samples.json)
+- various Bootstrap CSS, image and Jquery files
+
+## Getting Started
+
+To run this application simply launch the index.html file using live server or visit the hosted version [here](http://robdunnlab.com/projects/belly-button-biodiversity/ "The Public Science Lab").
--- static/js/app.js
+++ static/js/app.js
@@ -14,55 +14,51 @@ function init() {
             .append("option")
             .text(name);
         }));
-
-        var initial_ID = selection.property("value");
-        console.log("initial_ID= ",initial_ID);
-
+     
         // Call the BuildCharts function with the initial subject ID
+        var initial_ID = selection.property("value");
         BuildCharts(initial_ID);
        
     }));
 }
-
 //------------------------------------------
-// New Menu Item Selected
+// New Menu Item Selection
 //------------------------------------------
 // This function gets executed when a change is detected in the selected dropdown value
-// Note: it is referenced in the Index.html file
+// Note: it is invoked directly from the Index.html file
 function optionChanged(selectedID) {
-    console.log("Option changed - new subject ID: ", selectedID);
     // Clear out the previous subject's metadata from the card
     var oldMetadata = d3.select("ul", "#sample-metadata");
     oldMetadata.remove();
     // Build the Charts
     BuildCharts(selectedID);
 }
-    
 //------------------------------------------
 // Build the Charts
 //------------------------------------------ 
 //This function retrieves and prepares the necessary data for the subject id and builds the Plotly charts
 function BuildCharts(SubjectID) {
-
-     // Select the Metadata card in Index.html
-     var metadataCard = d3.select("#sample-metadata");
-    // Select the Bar Chart div in Index.html
-    var barChart = d3.select("#bar");
+  
     //------------------------------------------
     // Retrieve the Data
     //------------------------------------------
     // Read in JSON samples, getting the Subject ID from each one and using them to build the dropdown menu options
-   d3.json("samples.json").then((results => {
+    d3.json("samples.json").then((results => {
+       
        //------------------------------------------
        // Create the Metadata Card
        //------------------------------------------
+       // Select the Metadata card in Index.html
+       var metadataCard = d3.select("#sample-metadata");
+
        // Retrieve selected subjects metadata and grab the washing frequency for later
        var SubjectMetadata = results.metadata.filter(subject => subject.id == SubjectID)[0];
        var WashingFrequency = SubjectMetadata.wfreq;
-       console.log("SubjectMetadata= ", SubjectMetadata); 
+       
        // Create a list group in the Demographic Info card with some special styling  
        var metadataList = metadataCard.append("ul").attr("class", "list-group list-group-flush");
-       // Iterate through each key-value pair getting the values to build the Demographics panel
+
+       // Iterate through each key-value pair getting the values to build the Demographic Info card
        Object.entries(SubjectMetadata).forEach(([key, value]) => {
            var metadataItem = metadataList.append("li").
            attr("class", "list-group-item p-1 details-text").
@@ -96,12 +92,7 @@ function BuildCharts(SubjectID) {
                     break;
             }
         });
-
-        console.log("OTU_IDs: ", OTU_IDs);
-        console.log("OTU_labels: ", OTU_labels);
-        console.log("OTU_values: ", OTU_values);
-
-         //------------------------------------------
+        //------------------------------------------
         // Create the Bar Chart
         //------------------------------------------
         // Get the 10 specimens (OTUs) that occur most abundantly in this sample
@@ -113,10 +104,6 @@ function BuildCharts(SubjectID) {
 
         // Create String labels to use for the y axis of the bar chart
         var Top10_string_labels = Top10_IDs.map(OTU_ID => "OTU " + OTU_ID);
-
-        console.log("Top10_IDs: ", Top10_IDs);
-        console.log("Top10_labels: ", Top10_labels);
-        console.log("Top10_values: ", Top10_values);
 
         // Create the Plotly trace for the Bar chart
         var BarTrace = {
@@ -131,6 +118,8 @@ function BuildCharts(SubjectID) {
         };
        
         var BarData = [BarTrace];
+
+        // Create the Ploty layout for the Bar chart
         var BarLayout = {
             title:{
                 text: `<b>Top 10 Microbes in this Sample</b>`,
@@ -149,13 +138,13 @@ function BuildCharts(SubjectID) {
             height: 500
             };
         
-        // Plot the Bar Chart into the html div
+        // Plot the Bar Chart into the designated html div
         Plotly.newPlot("bar", BarData, BarLayout);
         
         //------------------------------------------
         // Create the Bubble Chart
         //------------------------------------------
-
+        // Create the Plotly Trace for the Bubble chart    
         var BubbleTrace = {
             x:  OTU_IDs[0],
             y:  OTU_values[0],
@@ -170,17 +159,19 @@ function BuildCharts(SubjectID) {
 
         var BubbleData = [BubbleTrace];
 
+        // Create the Ploty layout for the Bubble chart
         var BubbleLayout = {
             title: "Specimen Composition of this Sample",
             xaxis: { title: "OTU ID" }
          };
-        // Plot the Bubble Chart into the html div
+
+        // Plot the Bubble Chart into the designated html div
         Plotly.newPlot("bubble", BubbleData, BubbleLayout);
 
         //--------------------------------------------
         // Create the Gauge chart
         //--------------------------------------------
-        // Plot the washing frequency 
+        // Plot the washing frequency in a Gauge chart 
         var GaugeData = [
             {
               domain: { x: [0, 1], y: [0, 1] },
@@ -212,20 +203,11 @@ function BuildCharts(SubjectID) {
                 borderwidth: 2
             }}
         ];
-         
-                   
-          // gauge: { axis: { range: [null, 10] } }
-
-          var GaugeLayout = { width: 500, height: 450 };
-          Plotly.newPlot('gauge', GaugeData, GaugeLayout);
-   }));  
-
-
-    
-       
-
+           
+        var GaugeLayout = { width: 500, height: 450 };
+        Plotly.newPlot('gauge', GaugeData, GaugeLayout);
+   }));   
 }
-
 
 init();
 
